5053346221851734f474aee7212a8245f27de241
Алексей Юрьевич Нестеренко <anesterenko@hse.ru>
2023-12-08 19:16:32 +0300

    Исправлен механизм сборки документации для сайта

--- cmake/MakeDoc.cmake
+++ cmake/MakeDoc.cmake
@@ -40,12 +40,28 @@ endif()
 
 # -------------------------------------------------------------------------------------------------- #
 if( UNIX )
+
+  # configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/libakrypt-header.tex.in ${CMAKE_CURRENT_BINARY_DIR}/libakrypt-header.tex @ONLY )
+  #  file( APPEND ${script} "cd ${CMAKE_CURRENT_BINARY_DIR}/doc-akrypt/latex\n" )
+  #  file( APPEND ${script} "make\n" )
+  #  file( APPEND ${script} "cp ${CMAKE_CURRENT_BINARY_DIR}/doc-akrypt/latex/refman.pdf ${CMAKE_CURRENT_BINARY_DIR}/doc/libakrypt-api-${FULL_VERSION}.pdf\n" )
+  #  file( APPEND ${script} "cd ${CMAKE_CURRENT_BINARY_DIR}\n" )
+
+  # генерация sphinx
   if( SPHINX )
     configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/Makefile.in ${CMAKE_CURRENT_BINARY_DIR}/sphinx/Makefile @ONLY )
     configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/conf.py.in ${CMAKE_CURRENT_SOURCE_DIR}/doc/conf.py @ONLY )
 
     set( script ${CMAKE_CURRENT_BINARY_DIR}/make-doc-${FULL_VERSION}.sh )
-    file( WRITE ${script} "#/bin/bash\ncd ${CMAKE_CURRENT_BINARY_DIR}/sphinx \n" )
+    file( WRITE ${script} "#/bin/bash\n" )
+
+    if( DOXYGEN )
+      configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile.akrypt.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.akrypt @ONLY )
+      file( APPEND ${script} "doxygen Doxyfile.akrypt\n" )
+      message("-- Support for doxygen documenation added")
+    endif()
+
+    file( APPEND ${script} "cd ${CMAKE_CURRENT_BINARY_DIR}/sphinx \n" )
    # формируем каталог с собранной воедино документацией
     file( APPEND ${script} "mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/doc\n" )
    # строим красивый вывод в html
@@ -73,23 +89,6 @@ if( UNIX )
     add_custom_target( doc ${script} )
     message("-- Script for documentation is done (now \"make doc\" enabled)")
   endif()
-
-  # генерация документации с помощью Doxygen отключена
-  # поскольку исходные тексты всё ещё содержат ряд невыковыриваемых ошибок в синтаксисе
-  #
-  #if( DOXYGEN )
-  #  # doxygen найден и документация может быть сгенерирована
-  #  configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile.akrypt.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.akrypt @ONLY )
-  #  configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/doc/libakrypt-header.tex.in ${CMAKE_CURRENT_BINARY_DIR}/libakrypt-header.tex @ONLY )
-  #
-  #  file( APPEND ${script} "doxygen Doxyfile.akrypt\n" )
-  #  file( APPEND ${script} "cd ${CMAKE_CURRENT_BINARY_DIR}/doc-akrypt/latex\n" )
-  #  file( APPEND ${script} "make\n" )
-  #  file( APPEND ${script} "cp ${CMAKE_CURRENT_BINARY_DIR}/doc-akrypt/latex/refman.pdf ${CMAKE_CURRENT_BINARY_DIR}/doc/libakrypt-api-${FULL_VERSION}.pdf\n" )
-  #  file( APPEND ${script} "cd ${CMAKE_CURRENT_BINARY_DIR}\n" )
-  #
-  #  message("-- Support for doxygen documenation added")
-  #endif()
 
 endif()
 # -------------------------------------------------------------------------------------------------- #
